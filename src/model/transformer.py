2e4d233bd1adb25dabc74bbac1670c2488be536b
kirili4ik <kpgelvan@edu.hse.ru>
2021-03-17 18:21:08 +0300

    debug2 for beam search

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -163,8 +163,8 @@ class MultiHeadAttention(nn.Module):
 
         if kv is None and self.max_relative_positions > 0:
 
-            logger.info('query size', q.size())
-            logger.info('relations_k size', relations_k.size())
+            logger.info(q.size())
+            logger.info(relations_k.size())
 
             #print('query')
             #print(q)
@@ -224,9 +224,9 @@ class MultiHeadAttention(nn.Module):
             #print('weights or drop(softmax(mask(q_k))) size', weights.size())
             #print('weights or drop(softmax(mask(q_k)))')
             #print(weights)
-            logger.info('w1', weights.size())
-            logger.info('w2', weights[:, :, :, :weights.shape[-2]].size())
-            logger.info('relations_v size', relations_v.size())
+            logger.info(weights.size())
+            logger.info(weights[:, :, :, :weights.shape[-2]].size())
+            logger.info(relations_v.size())
 
             res2 = relative_matmul(weights[:, :, :, :weights.shape[-2]],
                                                 relations_v,
