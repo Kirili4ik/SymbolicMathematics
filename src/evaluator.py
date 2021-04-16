48b1bb2f0103b545345330ac61d3df73af0982ba
kirili4ik <kpgelvan@edu.hse.ru>
2021-04-16 14:44:07 +0300

    misspelling

--- src/evaluator.py
+++ src/evaluator.py
@@ -283,7 +283,7 @@ class Evaluator(object):
                 (x1, len1), (x2, len2), (rel_matrices_batch, rel_lens), nb_ops = elem
                 root_path_batch = None
             elif self.trainer.root_paths_path is not None:
-                (x1, len1), (x2, len2), root_path_batch, nd_ops = elem
+                (x1, len1), (x2, len2), root_path_batch, nb_ops = elem
                 rel_matrices_batch = None
                 rel_lens = None
             else:
