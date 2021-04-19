da944916392d2a640e6087e1279be58c99e4fc5f
kirili4ik <kpgelvan@edu.hse.ru>
2021-04-19 17:02:48 +0300

    debug15

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -490,6 +490,7 @@ class TransformerModel(nn.Module):
             attn_mask = attn_mask[:, -_slen:]
             rel_mask = rel_mask[:, -_slen:] if rel_mask is not None else None
             rel_matrix = rel_matrix[:, -_slen] if rel_matrix is not None else None
+            root_paths = root_paths[:, -_slen, :] if root_paths is not None else None
 
         # all layer outputs
         if TransformerModel.STORE_OUTPUTS and not self.training:
