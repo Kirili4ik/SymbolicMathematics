dd0505612af8f84a2a88ae95542bfbb8f8c7acf7
kirili4ik <kpgelvan@edu.hse.ru>
2021-05-07 04:01:30 +0300

    fix full seq rel att, but rip

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -424,7 +424,7 @@ class TransformerModel(nn.Module):
             self.layer_norm1.append(nn.LayerNorm(self.dim, eps=1e-12))
             if self.is_decoder:
                 self.layer_norm15.append(nn.LayerNorm(self.dim, eps=1e-12))
-                self.encoder_attn.append(MultiHeadAttention(self.n_heads, self.dim, dropout=self.attention_dropout))
+                self.encoder_attn.append(MultiHeadAttention(self.n_heads, self.dim, dropout=self.attention_dropout, use_encdec_seq_rel_att=self.use_encdec_seq_rel_att))
             self.ffns.append(TransformerFFN(self.dim, self.hidden_dim, self.dim, dropout=self.dropout))
             self.layer_norm2.append(nn.LayerNorm(self.dim, eps=1e-12))
 
