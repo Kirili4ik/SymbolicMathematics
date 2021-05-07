94b7a9c357c233f57fb196b9ca0c63b096f58fae
kirili4ik <kpgelvan@edu.hse.ru>
2021-05-07 04:13:53 +0300

    sctipt update

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -38,7 +38,7 @@ def build_modules(env, params):
     # reload pretrained modules
     if params.reload_model != '':
         logger.info(f"Reloading modules from {params.reload_model} ...")
-        reloaded = torch.load(params.reload_model)
+        reloaded = torch.load(params.reload_model, map_location='cpu' if params.cpu else 'cuda')
         for k, v in modules.items():
             assert k in reloaded
             if all([k2.startswith('module.') for k2 in reloaded[k].keys()]):
